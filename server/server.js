1bc5cefa9b277c617ce9b9088dedef07f47f0a7c
Paulo Ricardo Fernandes Gonçalves <35166619+Paulo1235@users.noreply.github.com>
2025-08-29 20:24:45 +0100

    Introduction
    feat: initialize fullstack project with server, client, and MongoDB connection
    
    - Set up backend with Express.js
    - Set up frontend with React
    - Installed and configured Mongoose for MongoDB Atlas connection
    - Created basic test route on the server
    - Added environment variables to store MongoDB URI

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config()
+const MONGO_URI = process.env.MONGO_URI;
+
+app.get("/", (req, res) => {
+  res.send("Servidor a funcionar!");
+});
+
+app.listen(5000, () => {
+  console.log("Servidor a funcionar em http://localhost:5000");
+});
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log("Ligado ao Mongo"))
+    .catch(() => console.log("Erro ao ligar ao Mongo"))
