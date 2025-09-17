aa2e5f20f01e9cae3272125410b3addf6444ec3d
Paulo Ricardo Fernandes Gonçalves <35166619+Paulo1235@users.noreply.github.com>
2025-09-17 10:37:07 +0100

    Refactor user controller and service logic
    Moved user-related operations from UserRepository to UserService in the controller. Added new methods to UserService for creating, editing, deleting, and finding users, and updated controller to use these methods. Fixed import in userRoutes to match controller filename.

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,10 +1,46 @@
-const UserRepository = require('../repositories/UserRepository')
+const UserService = require('../service/UserService')
 const { StatusCodes } = require('http-status-codes')
 
 class UserController {
+  static async createUser (req, res) {
+    try {
+      const users = await UserService.createUser(req.body)
+      res.json(users).status(StatusCodes.OK)
+    } catch (error) {
+      res.json(error).status(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  static async editUser (req, res) {
+    try {
+      const users = await UserService.editUser(req.body)
+      res.json(users).status(StatusCodes.OK)
+    } catch (error) {
+      res.json(error).status(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+  }
+
   static async findAllUsers (_, res) {
     try {
-      const users = await UserRepository.findAllUsers()
+      const users = await UserService.findAllUsers()
+      res.json(users).status(StatusCodes.OK)
+    } catch (error) {
+      res.json(error).status(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  static async deleteUser (req, res) {
+    try {
+      const users = await UserService.deleteUser(req.body)
+      res.json(users).status(StatusCodes.OK)
+    } catch (error) {
+      res.json(error).status(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  static async findUserById (req, res) {
+    try {
+      const users = await UserService.findUserById(req.body)
       res.json(users).status(StatusCodes.OK)
     } catch (error) {
       res.json(error).status(StatusCodes.INTERNAL_SERVER_ERROR)
--- server/src/service/UserService.js
+++ server/src/service/UserService.js
@@ -1,5 +1,6 @@
 //  const User = require('../models/User')
 const userSchema = require('../validations/userSchema')
+const UserRepository = require('../repositories/UserRepository')
 
 class UserService {
   static async createUser (data) {
@@ -7,7 +8,60 @@ class UserService {
     if (data.birthDate > new Date()) {
       throw new Error('Birth Date cannot be in the future')
     }
+
+    try {
+      UserRepository.createUser(validatedData)
+    } catch (error) {
+      throw new Error('Error creating User')
+    }
   }
-}//chama o repository
+
+  static async editUser (data) {
+    const validatedData = await userSchema.validate(data, { abortEarly: false })
+    if (data.birthDate > new Date()) {
+      throw new Error('Birth Date cannot be in the future')
+    }
+
+    try {
+      UserRepository.editUser(validatedData)
+    } catch (error) {
+      throw new Error('Error editting User')
+    }
+  }
+
+  static async findAllUsers () {
+    try {
+      UserRepository.findAllUsers()
+    } catch (error) {
+      throw new Error('Error finding all Users')
+    }
+  }
+
+  static async deleteUser (data) {
+    // const validatedData = await userSchema.validate(data, { abortEarly: false })
+    // if (data.birthDate > new Date()) {
+    //   throw new Error('Birth Date cannot be in the future')
+    // }
+
+    try {
+      UserRepository.deleteUser(data)
+    } catch (error) {
+      throw new Error('Error editting User')
+    }
+  }
+
+  static async findUserById (data) {
+    // const validatedData = await userSchema.validate(data, { abortEarly: false })
+    // if (data.birthDate > new Date()) {
+    //   throw new Error('Birth Date cannot be in the future')
+    // }
+
+    try {
+      UserRepository.findUserById(data)
+    } catch (error) {
+      throw new Error('Error editting User')
+    }
+  }
+}
 
 module.exports = UserService
